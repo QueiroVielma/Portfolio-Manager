2a36a5772d93f11eb0258ec2bf3c06e9be392d38
QueiroVielma <queigerar@gmail.com>
2024-10-21 20:04:59 -0300

    Create status enum

--- src/main/java/gestorPortfolio/enums/Status.java
+++ src/main/java/gestorPortfolio/enums/Status.java
@@ -1,4 +1,31 @@
 package gestorPortfolio.enums;
 
 public enum Status {
+    UNDER_REVIEW(1),
+    ANALYSIS_PERFORMED(2),
+    ANALYSIS_APPROVED(3),
+    STARTED(4),
+    PLANNED(5),
+    IN_PROGRESS(6),
+    TERMINATED(7),
+    CANCELED(8);
+
+    private final int value;
+
+    Status(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static Status fromValue(int value) {
+        for (Status status : Status.values()) {
+            if (status.value == value) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid status value: " + value);
+    }
 }
