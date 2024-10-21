6b2152d8431d2740d0716983f03a50c0778e4935
QueiroVielma <queigerar@gmail.com>
2024-10-21 20:04:24 -0300

    Create Position enum

--- src/main/java/gestorPortfolio/enums/Position.java
+++ src/main/java/gestorPortfolio/enums/Position.java
@@ -1,4 +1,26 @@
 package gestorPortfolio.enums;
 
 public enum Position {
+    ADMIN(1),
+    MANAGER(2),
+    MEMBER(3);
+
+    private final int value;
+
+    Position(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static Position fromValue(int value) {
+        for (Position position : Position.values()) {
+            if (position.value == value) {
+                return position;
+            }
+        }
+        throw new IllegalArgumentException("Invalid position value: " + value);
+    }
 }
