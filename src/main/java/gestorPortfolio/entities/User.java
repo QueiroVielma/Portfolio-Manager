884ce226f86a120d62c4a2ae98588804116194a0
QueiroVielma <queigerar@gmail.com>
2024-10-21 20:03:42 -0300

    Create User entity

--- src/main/java/gestorPortfolio/entities/User.java
+++ src/main/java/gestorPortfolio/entities/User.java
@@ -1,4 +1,31 @@
 package gestorPortfolio.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany (mappedBy = "members")
+    private List<Project> projectsMembers;
+
+    @OneToMany (mappedBy = "responsibleManager")
+    private List<Project> projectsManager;
 }
