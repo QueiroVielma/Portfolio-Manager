e3d1307a8c4d4933e6a8a2ff3515c583057bb8e2
QueiroVielma <queigerar@gmail.com>
2024-10-21 20:03:32 -0300

    Create Project entity

--- src/main/java/gestorPortfolio/entities/Project.java
+++ src/main/java/gestorPortfolio/entities/Project.java
@@ -1,4 +1,47 @@
 package gestorPortfolio.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate startDate;
+
+    private LocalDate expectedEEndDate;
+
+    private LocalDate actualEndDate;
+
+    private double totalBudget;
+
+    private String description;
+
+    private int status;
+
+    @ManyToMany
+    @JoinTable(
+            name = "projects_users",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List <User> members;
+
+    @ManyToOne
+    private User responsibleManager;
 }
