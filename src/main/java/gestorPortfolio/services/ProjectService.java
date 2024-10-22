d57d26d572bb8d4164c2c5b6798dba94c78a14ce
QueiroVielma <queigerar@gmail.com>
2024-10-22 15:39:40 -0300

    Project service

--- src/main/java/gestorPortfolio/services/ProjectService.java
+++ src/main/java/gestorPortfolio/services/ProjectService.java
@@ -1,4 +1,96 @@
 package gestorPortfolio.services;
 
+import gestorPortfolio.dto.project.*;
+import gestorPortfolio.entities.Project;
+import gestorPortfolio.entities.User;
+import gestorPortfolio.enums.Status;
+import gestorPortfolio.repositoies.ProjectRepository;
+import gestorPortfolio.repositoies.UserRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Autowired
+    UserRespository userRespository;
+
+    public ProjectResponse save(ProjectRequest projectRequest){
+        User manager= this.userRespository.findById(projectRequest.getManagerId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + projectRequest.getManagerId()));
+        if (manager.getPosition()!=2){
+            throw new IllegalArgumentException("User is not a manager: " + manager.getName());
+        }
+        Project project= projectRequest.project();
+        project.setStartDate(LocalDate.now());
+        project.setStatus(Status.UNDER_REVIEW.getValue());
+        project.setResponsibleManager(manager);
+        return ProjectResponse.project(this.projectRepository.save(project));
+    }
+
+    public ProjectResponse update(UpdateRequest projectRequest, long id){
+        Project project= this.findById(id);
+        project.setName(projectRequest.getNameProject());
+        project.setRisk(projectRequest.getRiskProject().getValue());
+        project.setTotalBudget(projectRequest.getBudget());
+        project.setDescription(projectRequest.getDescription());
+        project.setId(id);
+        return ProjectResponse.project(this.projectRepository.save(project));
+    }
+
+    public Project findById(Long id){
+        Project project=this.projectRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Project ID: " + id));
+        return project;
+    }
+
+    public List<ProjectResponse> findAll(){
+        List <Project> projects= this.projectRepository.findAll();
+        return projects.stream()
+                .map(ProjectResponse::project)
+                .collect(Collectors.toList());
+    }
+
+    public String delete(Long id){
+        Project project= this.findById(id);
+        if(project.getStatus()<4 || project.getStatus()==8 ) {
+            projectRepository.deleteById(id);
+            return "User deleted successfully";
+        }else {
+            return "It is not possible to delete the project.";
+        }
+    }
+
+    public String attributeMembersById(MenbersIdRequest menbersId, Long projectId){
+        List<User> menbers=userRespository.findAllById(menbersId.getMenbersId());
+        Project project= this.findById(projectId);
+        project.setMembers(menbers);
+        projectRepository.save(project);
+        return"Members added with success";
+    }
+
+    public String updateStatus(ProjectProgress projectProgress, Long projectId){
+        Project project= this.findById(projectId);
+        if(project.getStatus()>6){
+            return "project completed, estatus: "+ Status.fromValue(project.getStatus());
+        }
+        if (projectProgress.isProgress() && project.getStatus() < 7) {
+            project.setStatus(project.getStatus()+1);
+        } else {
+            project.setStatus(8);
+        }
+        if(project.getStatus()>6){
+            project.setActualEndDate(LocalDate.now());
+        }
+        projectRepository.save(project);
+        return "Status: "+ Status.fromValue(project.getStatus());
+    }
+
 }
---
+++ src/test/java/gestorPortfolio/servicesTests/ProjectServiceTest.java
@@ -0,0 +1,180 @@
+package gestorPortfolio.servicesTests;
+
+import gestorPortfolio.dto.project.MenbersIdRequest;
+import gestorPortfolio.dto.project.ProjectProgress;
+import gestorPortfolio.dto.project.ProjectRequest;
+import gestorPortfolio.dto.project.ProjectResponse;
+import gestorPortfolio.entities.Project;
+import gestorPortfolio.entities.User;
+import gestorPortfolio.enums.Risk;
+import gestorPortfolio.enums.Status;
+import gestorPortfolio.repositoies.ProjectRepository;
+import gestorPortfolio.repositoies.UserRespository;
+import gestorPortfolio.services.ProjectService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class ProjectServiceTest {
+    @MockBean
+    ProjectRepository projectRepository;
+    @MockBean
+    UserRespository userRespository;
+    @Autowired
+    ProjectService projectService;
+
+    private Project project;
+    private User user;
+
+    @BeforeEach
+    void setup(){
+
+        user = new User();
+        user.setId(1L);
+        user.setName("Manager");
+        user.setPosition(2);
+
+        project = new Project();
+        project.setId(1L);
+        project.setName("Test Project");
+        project.setStatus(Status.UNDER_REVIEW.getValue());
+        project.setMembers(new ArrayList<>());
+        project.setRisk(1);
+        project.setResponsibleManager(user);
+    }
+
+    @Test
+    void testSaveProjectSuccess() {
+        ProjectRequest projectRequest = new ProjectRequest();
+        projectRequest.setNameProject("New Website Development");
+        projectRequest.setExpectedEndDate(LocalDate.of(2024, 12, 31));
+        projectRequest.setManagerId(1L);
+        projectRequest.setBudget(50000.00);
+        projectRequest.setDescription("Develop a responsive website for the company's portfolio.");
+        projectRequest.setRiskProject(Risk.LOW);
+
+        when(userRespository.findById(1L)).thenReturn(Optional.of(user));
+        when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+        ProjectResponse response = projectService.save(projectRequest);
+
+        assertNotNull(response);
+        assertEquals("Test Project", response.getName());
+        assertEquals(Status.UNDER_REVIEW, response.getStatus());
+    }
+
+    @Test
+    void testSaveProjectInvalidManager() {
+        ProjectRequest projectRequest = new ProjectRequest();
+        projectRequest.setNameProject("New Website Development");
+        projectRequest.setExpectedEndDate(LocalDate.of(2024, 12, 31));
+        projectRequest.setManagerId(2L);
+        projectRequest.setBudget(50000.00);
+        projectRequest.setDescription("Develop a responsive website for the company's portfolio.");
+        projectRequest.setRiskProject(Risk.LOW);
+        when(userRespository.findById(1L)).thenReturn(Optional.of(user));
+        when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+        when(userRespository.findById(2L)).thenReturn(Optional.empty());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> projectService.save(projectRequest));
+
+        assertEquals("Invalid user ID: 2", exception.getMessage());
+    }
+
+    @Test
+    void testFindByIdSuccess() {
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+
+        Project result = projectService.findById(1L);
+
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+    }
+
+    @Test
+    void testFindByIdNotFound() {
+        when(projectRepository.findById(1L)).thenReturn(Optional.empty());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> projectService.findById(1L));
+
+        assertEquals("Invalid Project ID: 1", exception.getMessage());
+    }
+
+    @Test
+    void testDeleteProjectSuccess() {
+        project.setStatus(3);
+
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+
+        String result = projectService.delete(1L);
+
+        assertEquals("User deleted successfully", result);
+        verify(projectRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    void testDeleteProjectFailure() {
+        project.setStatus(5);
+
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+
+        String result = projectService.delete(1L);
+
+        assertEquals("It is not possible to delete the project.", result);
+        verify(projectRepository, never()).deleteById(anyLong());
+    }
+
+    @Test
+    public void testUpdateStatusSuccessProgress() {
+        ProjectProgress projectProgress = new ProjectProgress();
+        projectProgress.setProgress(true);
+        project.setStatus(5); // Status inicial
+
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+        when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+        String result = projectService.updateStatus(projectProgress, 1L);
+
+        assertEquals("Status: " + Status.fromValue(6), result);
+        verify(projectRepository).save(project);
+        assertEquals(6, project.getStatus());
+    }
+
+    @Test
+    public void testUpdateStatusSuccessCompleted() {
+        project.setStatus(7);
+
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+
+        String result = projectService.updateStatus(new ProjectProgress(), 1L);
+
+        assertEquals("project completed, estatus: " + Status.fromValue(7), result);
+        verify(projectRepository, never()).save(project);
+    }
+
+    @Test
+    public void testUpdateStatusFailNotFound() {
+        when(projectRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            projectService.updateStatus(new ProjectProgress(), 1L);
+        });
+    }
+
+}
